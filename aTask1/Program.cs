615adf99a5e08ba81431aded391826dd79b9e557
Fray <kozar20000@mail.ru>
2022-07-28 18:55:39 +0300

    sumNumbers

---
+++ aTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("\nВведите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = 0; i < N; i++)
+{
+    if (M < N)
+    Console.Write((M++) + ", ");
+    else
+    Console.Write((M++) + ". ");
+}
+Console.WriteLine();
